934aba9688b90f521f4249c8d89dad2237463832
Michael Hoser <michael.hoser@outlook.com>
2025-01-26 20:10:55 +0100

    Red Line Sample 1x 64x64

---
+++ iot/platform-io/hub75/Hub75DMA/src/main.cpp
@@ -0,0 +1,64 @@
+#include <Arduino.h>
+
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
+
+#define PANEL_RES_X 64 // Number of pixels wide of each INDIVIDUAL panel module.
+#define PANEL_RES_Y 64 // 32     // Number of pixels tall of each INDIVIDUAL panel module.
+#define PANEL_CHAIN 1  // Total number of panels chained one to another
+
+// https://github.com/mrcodetastic/ESP32-HUB75-MatrixPanel-DMA?tab=readme-ov-file#2-wiring-the-esp32-to-an-led-matrix-panel
+#define R1_PIN 25
+#define G1_PIN 26
+#define B1_PIN 27
+#define R2_PIN 14
+#define G2_PIN 12
+#define B2_PIN 13
+
+#define A_PIN 23
+#define B_PIN 19
+#define C_PIN 5
+#define D_PIN 17
+#define E_PIN 32 // -1 // IMPORTANT: Change to a valid pin if using a 64x64px panel.
+
+#define LAT_PIN 4
+#define OE_PIN 15
+#define CLK_PIN 16
+
+HUB75_I2S_CFG::i2s_pins _pins = {
+    R1_PIN, G1_PIN, B1_PIN,
+    R2_PIN, G2_PIN, B2_PIN,
+    A_PIN, B_PIN, C_PIN, D_PIN, E_PIN,
+    LAT_PIN, OE_PIN, CLK_PIN};
+
+HUB75_I2S_CFG mxconfig(
+    PANEL_RES_X, // module width
+    PANEL_RES_Y, // module height
+    PANEL_CHAIN, // Chain length
+    _pins        // pin mapping
+);
+
+MatrixPanel_I2S_DMA *dma_display = nullptr;
+
+void setup()
+{
+  // mxconfig.gpio.e = 18;
+  // mxconfig.clkphase = false;
+  // mxconfig.driver = HUB75_I2S_CFG::FM6126A;
+
+  // Display Setup
+  dma_display = new MatrixPanel_I2S_DMA(mxconfig);
+  dma_display->begin();
+  dma_display->setBrightness8(90); // 0-255
+  dma_display->clearScreen();
+
+  // myGREEN = dma_display->color565(0, 255, 0);
+  // dma_display->fillScreen(myGREEN);
+
+  // line red
+  dma_display->drawLine(0, 0, dma_display->width() - 1, dma_display->height() - 1, dma_display->color444(15, 0, 0));
+  delay(500);
+}
+
+void loop()
+{
+}
--- iot/platform-io/hub75/README.md
+++ iot/platform-io/hub75/README.md
@@ -5,3 +5,17 @@
 ![alt text](images/esp32.jpg)
 
 ![alt text](images/asdelivery-esp32.jpg)
+
+## Hub75
+
+* https://github.com/2dom/PxMatrix
+
+* DMA: https://github.com/mrcodetastic/ESP32-HUB75-MatrixPanel-DMA
+  * PINS: https://github.com/mrcodetastic/ESP32-HUB75-MatrixPanel-DMA/blob/master/src/platforms/esp32/esp32-default-pins.hpp
+
+# CP2102 Win 11 Treiber
+
+https://www.silabs.com/developer-tools/usb-to-uart-bridge-vcp-drivers?tab=downloads
+
+* Download "CP210x Universal Windows Driver", v11.4.0, 12/18/2024
+* Right click on the `silabser.inf` file and select "Install"
